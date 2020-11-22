30d3c274980c9ceb811b6b4be67f09d5b8eca5da
Vladimir <71945409+Bob-I-Ne@users.noreply.github.com>
2020-11-22 18:39:23 +0300

    Добавил 2 скрипта для персонажа

---
+++ CharacherAnimation2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLogic : MonoBehaviour
+{
+  [RequireComponent(typeof(Animator)]
+  private Animator Animator;
+
+  private void Awake()
+  {
+    	Animator = GetComponent<Animator>();
+  }
+  private void Update()
+  {
+    HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+    Animator.SetFloat("Speed", Mathf.Abs(HorizontalMove));
+    if (Input.GetButtonDown("Jump"))
+    {
+        Animator.SetBool("IsJumping", true);
+    }
+  }
+  public void IsLanding()
+  {
+      Animator.SetBool("IsJumping", false);
+  }
+}
---
+++ CharacherMover2D.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterMover2D : MonoBehaviour
+{
+	[RequireComponent(typeof(Rigidbody2D),typeof(BoxCollider2D))]
+	[SerializeField] private float RunSpeed = 40f;
+	[SerializeField] private float JumpForce = 400f;                          // Amount of force added when the player jumps.
+	[Range(0, .3f)] [SerializeField] private float MovementSmoothing = .05f;  // How much to smooth out the movement
+	[SerializeField] private LayerMask WhatIsGround;                          // A mask determining what is ground to the character
+	[SerializeField] private Transform GroundCheck;                           // A position marking where to check if the player is grounded.
+
+	private float HorizontalMove = 0f;
+	private bool Jump = false;
+
+	const float GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool Grounded;            // Whether or not the player is grounded.
+	private Rigidbody2D rigidbody2D;
+	private bool FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 Velocity = Vector3.zero;
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
+
+	private void Awake()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		if (OnLandEvent == null)
+		{
+			OnLandEvent = new UnityEvent();
+		}
+	}
+	private void Update()
+	{
+		HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+		if (Input.GetButtonDown("Jump"))
+		{
+				Jump = true;
+		}
+	}
+	private void FixedUpdate()
+	{
+		bool wasGrounded = Grounded;
+		Grounded = false;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, GroundedRadius, WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				Grounded = true;
+				if (!wasGrounded && rigidbody2D.velocity.y < 0)
+					OnLandEvent.Invoke();
+			}
+		}
+		Move(HorizontalMove * Time.fixedDeltaTime, Jump);
+		Jump = false;
+	}
+	public void Move(float move, bool jump)
+	{
+		Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
+		rigidbody2D.velocity = Vector3.SmoothDamp(rigidbody2D.velocity, targetVelocity, ref Velocity, MovementSmoothing);
+		if ((move > 0 && !FacingRight) || (move < 0 && FacingRight))
+		{
+			Flip();
+		}
+		if (Grounded && jump)
+		{
+			Grounded = false;
+			rigidbody2D.AddForce(new Vector2(0f, JumpForce));
+		}
+	}
+	private void Flip()
+	{
+		FacingRight = !FacingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
