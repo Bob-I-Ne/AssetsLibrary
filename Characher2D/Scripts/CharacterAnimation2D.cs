322180be6dbaf0cef4b177c4ce6a5ee899844e40
Vladimir <71945409+Bob-I-Ne@users.noreply.github.com>
2021-01-08 22:31:27 +0300

    Добавлен персонаж как таковой

---
+++ Characher2D/Scripts/CharacterAnimation2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class CharacterAnimation2D : MonoBehaviour
+{
+  private Animator Animator;
+
+  private void Awake()
+  {
+    	Animator = GetComponent<Animator>();
+  }
+  private void Update()
+  {
+    Animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+    if (Input.GetButtonDown("Jump"))
+    {
+        Animator.SetBool("IsJumping", true);
+    }
+  }
+  public void IsLanding()
+  {
+      Animator.SetBool("IsJumping", false);
+  }
+}
--- CharacherAnimation2D/CharacterAnimation2D.cs
+++ CharacherAnimation2D/CharacterAnimation2D.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator)]
+[RequireComponent(typeof(Animator))]
 
 public class CharacterAnimation2D : MonoBehaviour
 {
